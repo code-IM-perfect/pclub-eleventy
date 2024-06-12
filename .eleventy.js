a0b6ef8312aa7aa2d0505da5c2e788822f9f882a
Harshit Tomar <harshitt345@gmail.com>
2024-06-12 15:29:44 +0530

    Initial Commit

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function (eleventyConfig) {
+eleventyConfig.addPassthroughCopy("./src/styles");
+eleventyConfig.addWatchTarget("./src/styles");
+
+	return {
+		dir: {
+			input: "src",
+			output: "public",
+		},
+	};
+};
