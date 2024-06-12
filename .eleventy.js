da1be8330c1e8dfce019bedc015b47e71edc749f
Harshit Tomar <harshitt345@gmail.com>
2024-06-12 16:45:01 +0530

    added images
    Signed-off-by: Harshit Tomar <harshitt345@gmail.com>

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function (eleventyConfig) {
 eleventyConfig.addPassthroughCopy("./src/styles");
 eleventyConfig.addWatchTarget("./src/styles");
 
+eleventyConfig.addPassthroughCopy("./src/images/");
+
 	return {
 		dir: {
 			input: "src",
