7661cc9113895d7c93f8d06e03c069908ff2a004
Harshit Tomar <harshitt345@gmail.com>
2024-09-24 16:42:10 +0530

    favicon

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/scripts");
   eleventyConfig.addWatchTarget("./src/scripts");
 
+  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
   eleventyConfig.addPassthroughCopy("./src/images/");
 
   return {
